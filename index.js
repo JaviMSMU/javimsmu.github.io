2ec8861f1d246f0a311cb2ecf6ea531be816a5bd
Javier Crevoisier <j.j.crevoisier@email.msmary.edu>
2025-04-29 16:39:47 -0400

    version 2: no major changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {  // Triggering shrink after 50px scroll
+window.onscroll = function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 100) {
         navbar.classList.add('shrink');
     } else {
         navbar.classList.remove('shrink');
     }
-});
+};
 
 let currentIndex = 0;
 const slides = document.querySelectorAll('.slideshow-container .slide');
