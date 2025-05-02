7e44bdc828dc88d556517af8ab7c3bd82c237178
Javier Crevoisier <j.j.crevoisier@email.msmary.edu>
2025-05-02 17:03:37 -0400

    version 3: no major changes

--- cities.js
+++ cities.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
   const navbar = document.querySelector('.navbar');
-  if (window.scrollY > 50) {  // Triggering shrink after 50px scroll
+  if (window.scrollY > 50) {
       navbar.classList.add('shrink');
   } else {
       navbar.classList.remove('shrink');
