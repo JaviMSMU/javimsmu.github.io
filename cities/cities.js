10a9e08c7226a5d48d77ef54f22e72cf2877b80e
Javier Crevoisier <j.j.crevoisier@email.msmary.edu>
2025-05-05 19:31:27 -0400

    version 4: final javascript version, no major changes, js

--- cities/cities.js
+++ cities/cities.js
@@ -1,10 +1,10 @@
 window.addEventListener('scroll', function() {
-  const navbar = document.querySelector('.navbar');
-  if (window.scrollY > 50) {
-      navbar.classList.add('shrink');
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 20) {
+        navbar.classList.add('shrink');
   } else {
-      navbar.classList.remove('shrink');
-  }
+        navbar.classList.remove('shrink');
+    }
 });
 
 const searchInput = document.getElementById("searchInput");
